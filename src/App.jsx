f321d9f0fff726faf3a6bc05a2232f3cca20bd75
Antonio <antoniojacintoa@gmail.com>
2025-09-01 15:38:10 -0300

    criando componente tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import Botao from "./components/Botao"
+import Tela from "./components/Tela"
 
 function App() {
   return (
-    <section className="h-screen flex justify-center items-center">
-      <section className="bg-blue-900 p-5 flex gap-2 rounded-2xl">
-        <section className=" grid grid-cols-3 grid-rows-4 gap-2 ">
-          <Botao label={"1"} />
-          <Botao label={"2"} />
-          <Botao label={"3"} />
-          <Botao label={"4"} />
-          <Botao label={"5"} />
-          <Botao label={"6"} />
-          <Botao label={"7"} />
-          <Botao label={"8"} />
-          <Botao label={"9"} />
-          <Botao label={"0"} />
-          <Botao label={"AC"} />
-          <Botao label={"="} />
-        </section>
-        <section className="grid grid-cols-1 grid-rows-4 gap-2">
-          <Botao label={"+"} />
-          <Botao label={"-"} />
-          <Botao label={"*"} />
-          <Botao label={"/"} />
+    <section className="h-screen flex flex-col justify-center items-center">
+      <section className="bg-blue-900 p-5 flex flex-col gap-2 rounded-2xl">
+        <Tela resultado="0" />
+        <section className="flex gap-2">
+          <section className=" grid grid-cols-3 grid-rows-4 gap-2 ">
+            <Botao label={"1"} />
+            <Botao label={"2"} />
+            <Botao label={"3"} />
+            <Botao label={"4"} />
+            <Botao label={"5"} />
+            <Botao label={"6"} />
+            <Botao label={"7"} />
+            <Botao label={"8"} />
+            <Botao label={"9"} />
+            <Botao label={"0"} />
+            <Botao label={"AC"} />
+            <Botao label={"="} />
+          </section>
+          <section className="grid grid-cols-1 grid-rows-4 gap-2">
+            <Botao label={"+"} />
+            <Botao label={"-"} />
+            <Botao label={"*"} />
+            <Botao label={"/"} />
+          </section>
         </section>
       </section>
     </section>
---
+++ src/components/Tela.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Tela = ({ resultado }) => {
+  return (
+    <div>
+        <span>
+            { resultado }
+        </span>
+    </div>
+  )
+}
+
+export default Tela

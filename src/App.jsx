c113b18d02e615ebccd5ae0e26c79b9bc7fb6b7d
Antonio <antoniojacintoa@gmail.com>
2025-09-01 14:27:32 -0300

    estruturando e estilizando app

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,30 @@ import Botao from "./components/Botao"
 
 function App() {
   return (
-    <div>
-      <Botao label={"1"}/>
-    </div>
+    <section className="h-screen flex justify-center items-center">
+      <section className="bg-blue-900 p-5 flex gap-2 rounded-2xl">
+        <section className=" grid grid-cols-3 grid-rows-4 gap-2 ">
+          <Botao label={"1"} />
+          <Botao label={"2"} />
+          <Botao label={"3"} />
+          <Botao label={"4"} />
+          <Botao label={"5"} />
+          <Botao label={"6"} />
+          <Botao label={"7"} />
+          <Botao label={"8"} />
+          <Botao label={"9"} />
+          <Botao label={"0"} />
+          <Botao label={"AC"} />
+          <Botao label={"="} />
+        </section>
+        <section className="grid grid-cols-1 grid-rows-4 gap-2">
+          <Botao label={"+"} />
+          <Botao label={"-"} />
+          <Botao label={"*"} />
+          <Botao label={"/"} />
+        </section>
+      </section>
+    </section>
   )
 }
 

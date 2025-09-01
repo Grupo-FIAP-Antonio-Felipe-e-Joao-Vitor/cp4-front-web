2e3b2107716d8602d1827453691a67cf8d031043
Antonio <antoniojacintoa@gmail.com>
2025-09-01 15:39:13 -0300

    estilizando tela

--- src/components/Tela.jsx
+++ src/components/Tela.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Tela = ({ resultado }) => {
   return (
-    <div>
-        <span>
+    <div className='bg-black flex justify-end pt-1 pb-1 pr-3'>
+        <span className='text-white text-3xl'>
             { resultado }
         </span>
     </div>
